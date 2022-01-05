6c0fc84b91b9243519cdf5ddad3590680773e643
jgomezfl <jgomezfl@unal.edu.co>
2022-01-05 11:28:34 -0500

    update

---
+++ Streaming_Exercise/src/app.js
@@ -0,0 +1,24 @@
+//Guardamos en una constante ela libreria express
+const express = require('express'); 
+const { Socket } = require('socket.io');
+//Guardamos en una constante llamada app el resultado de la función express
+//Este objeto app es el que me permite iniciar el servidor
+const app = express();
+//Creamos un servidor http a partir de la libreria express, para todas las conexiones que necesitamos
+const http = require('http').Server(app);
+//Usamos socket.io para generar una conexión bidireccional en tiempo real entre clientes y servidores web
+const io = require('socket.io')(http);
+//A continuación generamos las rutas
+app.use(require('./routes/Streaminng_Exercise.routes'));
+//Luego le decimos donde vamos a cargar los archivos estaticos
+app.use(express.static(__dirname+"/public"))
+//Creamos un evento para generar una conexion, estamos  creando un canal de comunicación
+io.on('connection', (socket) =>{
+    //Ahora genramos un evento 'stream' que enviará la imagen
+    socket.on('stream', (image) => {
+        //Emitimos el evento, la imagen la capturaremos desde el fichero html
+        socket.broadcast.emit('stream', image);
+    })
+})
+//Exportamos http para poder utilizarlo desde index.js
+module.exports = http;
