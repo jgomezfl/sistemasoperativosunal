3d75040fa00b7b987587585849e8e5aa6735d351
jgomezfl <92111358+jgomezfl@users.noreply.github.com>
2022-01-18 09:04:18 -0500

    update

--- Streaming_Chat/server.js
+++ Streaming_Chat/server.js
@@ -1,8 +1,10 @@
+//Guardamos en la constante app la libreria express
 const app = require('express')();
+//A continuación a partir de la libreria express creamos un servido http
 const serverHTTP = require('http').Server(app);
-
+//Creamos una lista para almacenar los emensajes
 const Messages = []
-
+//La construcción de esta variable io es de esta forma para permitir el acceso de cualquier origen
 io = require('socket.io')(serverHTTP, {
     cors: {
       origin: '*',
